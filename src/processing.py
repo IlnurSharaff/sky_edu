a034b5a769ea9d076b69b7253c4a7f9ca31ac610
илнур шарафуллин <shir-tn@mail.ru>
2024-08-25 10:59:03 +0300

    проверка processing.py на mypy, flake8, black и isort

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,12 @@
-def filter_by_state(base_list: list[dict], state: str = 'EXECUTED') -> list[dict]:
-    '''возврат списка словарей по заданному значению ключа state'''
+def filter_by_state(base_list: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """возврат списка словарей по заданному значению ключа state"""
     filtered_list = []
     for dictionary in base_list:
         if dictionary["state"] == state:
             filtered_list.append(dictionary)
     return filtered_list
 
+
 def sort_by_date(base_list: list[dict], sort_type: bool = True) -> list[dict]:
-    '''сортировка списка словарей по дате, по умолчанию - убывающий'''
-    return sorted(base_list, key = lambda x:x['date'], reverse = sort_type)
+    """сортировка списка словарей по дате, по умолчанию - убывающий"""
+    return sorted(base_list, key=lambda x: x["date"], reverse=sort_type)
