aae8974f6323c3a317e4918d02bcf96668963a31
илнур шарафуллин <shir-tn@mail.ru>
2024-08-31 00:53:25 +0300

    внесение исправлений

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -2,31 +2,10 @@ import pytest
 
 from src.masks import get_mask_card_number, get_mask_account
 
-@pytest.fixture
-def card_mask_correct_number():
-    return 7000792289606361
 
-@pytest.fixture
-def card_mask_correct_answer():
-    return '7000 79** **** 6361'
-
-@pytest.fixture
-def card_mask_incorrect_number():
-    return 47000792289606361
-
-@pytest.fixture
-def card_mask_account_correct_number():
-    return 73654108430135874305
-
-@pytest.fixture
-def card_mask_account_correct_answer():
-    return '**4305'
-
-@pytest.fixture
-def card_mask_account_incorrect_number():
-    return 54108430135874305
 
 def test_get_mask_card_number(card_mask_correct_number, card_mask_correct_answer, card_mask_incorrect_number):
+
     assert get_mask_card_number(card_mask_correct_number) == card_mask_correct_answer
     with pytest.raises(ValueError):
         get_mask_card_number(card_mask_incorrect_number)
